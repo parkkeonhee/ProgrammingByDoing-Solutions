95fba5e8f3d861f95f92aebd66c05492c7823cf5
parkkeonhee <parkkeonhee@gmail.com>
2016-11-12 01:43:10 -0500

    more stuff

---
+++ FlipAgain.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class FlipAgain {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random generator = new Random();
+		String response, flippedCoin = "";
+		int numberOfHeads = 0, numberOfTails = 0;
+		int flippedNumber;
+		do {
+			flippedNumber = generator.nextInt(2);
+			flippedCoin = flippedNumber == 1 ? "HEADS" : "TAILS";
+			if (flippedNumber == 1) {
+				numberOfHeads++;
+			} else {
+				numberOfTails++;
+			}
+			System.out.print("You flip a coin and it is... " + flippedCoin);
+			System.out.println();
+			System.out.print("Would you like to flip again (y/n)? ");
+			response = sc.nextLine();
+		} while (response.equalsIgnoreCase("y"));
+		sc.close();
+
+		System.out.println("There were " + numberOfHeads + " heads, " + numberOfTails + " tails, and total of "
+				+ (numberOfTails + numberOfHeads) + " coin flips.");
+	}
+}
