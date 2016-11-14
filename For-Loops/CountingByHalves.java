5f669c59e917484ef8dc2af9cc2cd31647963bdd
Keonhee <parkkeonhee@users.noreply.github.com>
2016-11-13 23:04:00 -0500

    Add files via upload

---
+++ For-Loops/CountingByHalves.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Keon Hee Park
+ * Assignment: http://programmingbydoing.com/a/counting-by-halves.html
+ * Completed: November 13, 2016
+ */
+public class CountingByHalves {
+	public static void main(String[] args) {
+		System.out.println("x");
+		System.out.println("------");
+		for (double i = -10; i <= 10.0; i += 0.5) {
+			System.out.println(i);
+		}
+	}
+}
